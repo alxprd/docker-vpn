0aabec8e39275e19c74171841b2c87f32da6cefe
Alejandro Pereda <atk1005@gmail.com>
2018-09-09 15:18:22 +0000

    Allows to export files without root permissions.

--- export_secret.sh
+++ export_secret.sh
@@ -12,9 +12,11 @@ if [ ! -d "$PWD/conf/servers/$server_name" ]; then
         exit 0
 fi
 
-export_dir=$PWD/conf/servers/${server_name}_export
+export_dir=$PWD/exported/servers/${server_name}
+export_file=$export_dir/ta.key
 
 mkdir -p $export_dir
-rm -f $export_dir/ta.key
-cp $PWD/conf/servers/$server_name/ta.key $export_dir/ta.key
-chown -R $USER:$USER $export_dir/
+rm -f $export_file
+echo "$(sudo cat $PWD/conf/servers/$server_name/ta.key)" >> $export_file
+#cp --no-preserve=mode,ownership $PWD/conf/servers/$server_name/ta.key $export_dir/ta.key
+#chown -R $USER:$USER $export_dir/
