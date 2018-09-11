b2c1b532e8ed4950dc962d15832763a283012ea1
Alejandro Pereda <atk1005@gmail.com>
2018-09-12 04:41:26 +0800

    Packs the server config files in a zip file and uses the zip to start a server.

--- setup_ca_server.sh
+++ setup_ca_server.sh
@@ -7,4 +7,7 @@ if [ -z "${server_name}" ]; then
 	exit 0
 fi
 
-docker run --name ca-setup-server --rm -v $PWD/conf/keys:/root/ca/keys -v $PWD/conf/servers:/root/servers -it alxprd/ca setup-server $server_name
+docker run --name ca-setup-server --rm \
+	-v $PWD/conf/keys:/root/ca \
+	-v $PWD/data:/root/output \
+	-it alxprd/ca setup-server $server_name
--- start_server.sh
+++ start_server.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-server_name=$1
+server_zip_path=$1
 
-if [ -z "${server_name}" ]; then
-        echo "Asign a valid server name!"
-        exit 0
+# If it's a relative path adds $PWD to make it work with Docker
+if [ "${server_zip_path:0:1}" != "/" ]; then
+	server_zip_path=$PWD/$server_zip_path
 fi
 
-if [ ! -d "$PWD/conf/servers/$server_name" ]; then
-	echo "Server '$server_name' doesn't exist!"
+if [ ! -f "$server_zip_path" ]; then
+	echo "Server zip '$server_zip_path' doesn't exist!"
 	exit 0
 fi
 
+server_name=$(basename "${server_zip_path%.*}")
+
 docker run --name "vpn-server-${server_name}" --rm --privileged -p 1194:1194/udp \
-	-v $PWD/conf/servers/$server_name:/etc/openvpn/server:ro \
-	-v $PWD/conf/server.conf:/etc/openvpn/server.conf:ro \
+	-v $server_zip_path:/root/server.zip:ro \
 	-d alxprd/vpn-server
