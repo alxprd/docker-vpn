4ede69880779911e7afef2a7189555ac62815704
Alejandro Pereda <atk1005@gmail.com>
2018-09-13 20:24:49 +0800

    Some fixes into server files.

--- setup_ca_server.sh
+++ setup_ca_server.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-server_name=$1
-
-if [ -z "${server_name}" ]; then
-	echo "Asign a valid server name!"
-	exit 0
-fi
-
 docker run --name ca-setup-server --rm \
-	-v $PWD/conf/keys:/root/ca \
+	-v $PWD/conf/keys:/root/ca/keys \
 	-v $PWD/data:/root/output \
-	-it alxprd/ca setup-server $server_name
+	-it alxprd/vpn-ca setup-server "$@"
