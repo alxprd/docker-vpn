6e78a1e97e651f700f85dd8ab42b927bd691e8e5
Alejandro Pereda <atk1005@gmail.com>
2018-10-17 05:08:45 +0800

    Removes unneded parameters.

--- start-monitor.sh
+++ start-monitor.sh
@@ -2,19 +2,21 @@
 
 echo_template() {
 	echo "Help: ./start-monitor.sh -h"
-	echo "Use:  ./start-monitor.sh -c <config_monitor_path> -s <server_name> ..."
+#	echo "Use:  ./start-monitor.sh -c <config_monitor_path> -s <server_name> ..."
+	echo "Use:  ./start-monitor.sh -c <config_monitor_path> ..."
 }
 
 # Default param values:
 config_monitor_path=''
-server_name=''
+#server_name=''
 
-while getopts 'hc:s:' optp
+#while getopts 'hc:s:' optp
+while getopts 'hc:' optp
 do
 	case $optp in
 		h) echo_template; exit 0 ;;
 		c) config_monitor_path=$OPTARG ;;
-		s) server_name=$OPTARG ;;
+#		s) server_name=$OPTARG ;;
 	esac
 done
 
@@ -26,11 +28,11 @@ if [ -z "${config_monitor_path}" ]; then
 	exit 0
 fi
 
-if [ -z "${server_name}" ]; then
-	echo "Asign a valid server name!"
-	echo_template
-	exit 0
-fi
+#if [ -z "${server_name}" ]; then
+#	echo "Asign a valid server name!"
+#	echo_template
+#	exit 0
+#fi
 
 # If it's a relative path adds $PWD to make it work with Docker
 case $config_monitor_path in
@@ -43,7 +45,8 @@ if [ ! -f "$config_monitor_path" ]; then
 	exit 0
 fi
 
-docker run --name "vpn-monitor-${server_name}" --rm -p 80:80 \
+#docker run --name "vpn-monitor-${server_name}" --rm -p 80:80 \
+docker run --name "vpn-monitor" --rm -p 80:80 \
 	-v $config_monitor_path:/var/www/html/openvpn-monitor/openvpn-monitor.conf:ro \
 	-d alxprd/vpn:monitor
 
