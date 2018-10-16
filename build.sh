2c3cb0ef7d4e5eba5304a28aea2739e342567858
Alejandro Pereda <atk1005@gmail.com>
2018-10-17 06:29:50 +0800

    Adds Grafana Dockerfile.

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ docker build -t alxprd/vpn:ca      -f ./docker/Dockerfile_ovpn.ca      ./docker/
 docker build -t alxprd/vpn:server  -f ./docker/Dockerfile_ovpn.server  ./docker/
 docker build -t alxprd/vpn:monitor -f ./docker/Dockerfile_ovpn.monitor ./docker/
 docker build -t alxprd/vpn:client  -f ./docker/Dockerfile_ovpn.client  ./docker/
+
+docker build -t alxprd/vpn:monitor-grafana -f ./docker/Dockerfile_ovpn.monitor-grafana \
+  --build-arg "GRAFANA_VERSION=latest" \
+  --build-arg "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource" \
+  ./docker/
--- start-monitor-grafana.sh
+++ start-monitor-grafana.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-docker run --name "vpn-monitor-grafana" --rm -p 3000:3000 \
-  -it grafana/grafana
+#mkdir -p $PWD/data/grafana # creates a folder for your data
+ID=$(id -u) # saves your user id in the ID variable
+
+# starts grafana with your user id and using the data folder
+#docker run --name "vpn-monitor-grafana" --rm -p 3000:3000 \
+#  -d alxprd/vpn:monitor-grafana
+
+docker run --name "vpn-monitor-grafana" --user $ID --rm -p 3000:3000 \
+  -v "$PWD/data/grafana:/var/lib/grafana" \
+  -d alxprd/vpn:monitor-grafana
