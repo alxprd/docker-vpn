080eebc1d059ad6bc3578234ac734faeefee8d38
Alejandro Pereda <atk1005@gmail.com>
2018-11-19 01:57:43 +0800

    Fixes config error.

--- start-collectd-test.sh
+++ start-collectd-test.sh
@@ -4,12 +4,12 @@ mkdir -p $PWD/config/collectd
 
 if [ ! -f "$PWD/config/collectd/collectd.conf" ]; then
   echo "Create default 'collectd.conf' file"
-  docker run --rm alxprd/collectd "cat /etc/collectd/collectd.conf" > $PWD/config/collectd/collectd.conf
+  docker run --rm alxprd/collectd cat /etc/collectd/collectd.conf > $PWD/config/collectd/collectd.conf
 fi
 
 if [ ! -f "$PWD/config/collectd/types.db" ]; then
   echo "Create default 'types.db' file"
-  docker run --rm alxprd/collectd "cat /usr/share/collectd/types.db" > $PWD/config/collectd/types.db
+  docker run --rm alxprd/collectd cat /usr/share/collectd/types.db > $PWD/config/collectd/types.db
 fi
 
 docker run --name "collectd-test" --rm \
