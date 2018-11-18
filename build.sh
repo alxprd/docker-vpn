2e9811a4cc24844f9fce992d1a7da62fc8a28909
Alejandro Pereda <atk1005@gmail.com>
2018-11-19 01:14:24 +0800

    Adds Graphite.

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ docker build -t alxprd/vpn:ca      -f ./docker/Dockerfile_ovpn.ca      ./docker/
 docker build -t alxprd/vpn:server  -f ./docker/Dockerfile_ovpn.server  ./docker/
 docker build -t alxprd/vpn:monitor -f ./docker/Dockerfile_ovpn.monitor ./docker/
 docker build -t alxprd/vpn:client  -f ./docker/Dockerfile_ovpn.client  ./docker/
+
+docker pull graphiteapp/graphite-statsd
---
+++ start-viewer-graphite.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+docker run --name "graphite" --rm \
+  -p 80:80 \
+  -p 2003-2004:2003-2004 \
+  -p 2023-2024:2023-2024 \
+  -p 8125:8125/udp \
+  -p 8126:8126 \
+  -v $PWD/config/graphite:/opt/graphite/conf \
+  -v $PWD/data/graphite:/opt/graphite/storage \
+  -v $PWD/config/statsd:/opt/statsd \
+  -it graphiteapp/graphite-statsd
+
+#  --restart=always \
